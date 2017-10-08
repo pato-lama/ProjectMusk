ead270d98af2c458cc988c4ffe791fc033ab2aff
Pato <patricio.lamacchia@gmail.com>
2017-10-08 16:33:16 -0500

    AI and Player Death
    Modified bullet and AI script to make enemies die and player respawn when health reaches 0.

--- Assets/Scripts/EnemyAI1.cs
+++ Assets/Scripts/EnemyAI1.cs
@@ -113,5 +113,11 @@ public class EnemyAI1 : MonoBehaviour
             return;
         }
     }
-
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+            health.takeDamage(10);
+    }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public bool destroyOnDeath;
+
+    public void takeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+                Destroy(gameObject);
+            else
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+        }
+    }
+
+    void RpcRespawn()
+    {
+            // move back to zero location
+        transform.position = Vector3.zero;
+        
+    }
+}
