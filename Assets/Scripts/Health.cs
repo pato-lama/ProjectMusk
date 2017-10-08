fddf5da3cc473dcf7c056785f36fc855161f7f15
Pato <patricio.lamacchia@gmail.com>
2017-10-08 17:14:10 -0500

    Minor Commit
    Minor

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Health : MonoBehaviour {
     public const int maxHealth = 100;
     public int currentHealth = maxHealth;
     public bool destroyOnDeath;
+    public RectTransform healthBar;
 
     public void takeDamage(int amount)
     {
         currentHealth -= amount;
+       
         if (currentHealth <= 0)
         {
             if (destroyOnDeath)
@@ -20,6 +24,13 @@ public class Health : MonoBehaviour {
                 RpcRespawn();
             }
         }
+        if (!destroyOnDeath)
+            onChangeHealth(currentHealth);
+    }
+
+    void onChangeHealth (int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
     }
 
     void RpcRespawn()
